bd795dcccfb5c371ad738ee34f985d3f15b28575
kurokuman <kurosawa.tomoyuki0812@gmail.com>
2020-06-13 22:26:10 +0900

    fix main

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	uuid "github.com/satori/go.uuid"
 )
 
 func main() {
 	fmt.Println(SumOfNum([]int{1, 2, 3, 4}))
-	u1 := uuid.Must(uuid.NewV4(), nil)
-	fmt.Println(u1)
 }
 
 func SumOfNum(list []int) int {
