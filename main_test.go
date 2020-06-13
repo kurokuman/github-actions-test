a006861bffc8b69ca197326652b823c6bdb881bf
kurokuman <kurosawa.tomoyuki0812@gmail.com>
2020-06-13 21:53:27 +0900

    change

--- main_test.go
+++ main_test.go
@@ -1,13 +1,18 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestsumOfNum(t *testing.T) {
-	input := []int{1, 2, 3}
+	input := []int{1, 2, 3, 4, 5}
 	act := sumOfNum(input)
-	ext := 6
+	ext := 15
 
 	if act != ext {
 		t.Fatal("error")
+	} else {
+		fmt.Println(act)
 	}
 }
